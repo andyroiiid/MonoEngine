e50ab210a3bca6cb7f6197a1665be987c6ca8d77
Andrew Huang <andyroiiid@live.com>
2023-02-12 04:52:20 -0600

    TextureGridAtlas

--- MonoEngineCSharp/BitmapFont.cs
+++ MonoEngineCSharp/BitmapFont.cs
@@ -6,29 +6,11 @@ namespace MonoEngine
 {
     public class BitmapFont
     {
-        private const int NumCols = 16;
-        private const int NumRows = 16;
-        private const int NumGrids = NumCols * NumRows;
-        private readonly static Vector2 GridUvSize = new Vector2(1.0f / NumCols, 1.0f / NumRows);
-        private readonly static Rect[] Grids = new Rect[NumGrids];
-
-        static BitmapFont()
-        {
-            for (var c = 0; c < NumGrids; c++)
-            {
-                var col = c % NumCols;
-                var row = NumRows - 1 - c / NumCols;
-                Grids[c] = new Rect(new Vector2(col * GridUvSize.X, row * GridUvSize.Y), GridUvSize);
-            }
-        }
-
-        private readonly Texture _texture;
-        private readonly Vector2 _charPixelSize;
+        private readonly TextureGridAtlas _atlas;
 
         public BitmapFont(Texture texture)
         {
-            _texture = texture;
-            _charPixelSize = _texture.Size * GridUvSize;
+            _atlas = new TextureGridAtlas(texture, 16, 16);
         }
 
         private Vertex2D[] BuildTextVertices(string text, Vector2 position, in Color color)
@@ -41,14 +23,14 @@ namespace MonoEngine
                 {
                     VertexUtils.BuildRectTriangles(
                         new ArraySegment<Vertex2D>(vertices, offset, 6),
-                        new Rect(position, _charPixelSize),
-                        Grids[c],
+                        new Rect(position, _atlas.GridPixelSize),
+                        _atlas.GetGridUvRect(c),
                         color
                     );
                     offset += 6;
                 }
 
-                position.X += _charPixelSize.X;
+                position.X += _atlas.GridPixelSize.X;
             }
 
             return vertices;
@@ -56,7 +38,7 @@ namespace MonoEngine
 
         public void DrawText(string text, in Vector2 position, in Color color)
         {
-            _texture.Bind(0);
+            _atlas.Texture.Bind(0);
             Renderer2D.DrawTriangles(BuildTextVertices(text, position, color));
         }
     }
