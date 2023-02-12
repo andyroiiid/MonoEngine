86c36b487f587c7ca28d8b675a01dde6847884fd
Andrew Huang <andyroiiid@live.com>
2023-02-12 02:44:38 -0600

    Simplify builtin assets

--- Game/Assets.Designer.cs
+++ Game/Assets.Designer.cs
@@ -21,14 +21,14 @@ using System;
 [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
 [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
 [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-internal class BuiltinAssets {
+internal class Assets {
     
     private static global::System.Resources.ResourceManager resourceMan;
     
     private static global::System.Globalization.CultureInfo resourceCulture;
     
     [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-    internal BuiltinAssets() {
+    internal Assets() {
     }
     
     /// <summary>
@@ -38,7 +38,7 @@ internal class BuiltinAssets {
     internal static global::System.Resources.ResourceManager ResourceManager {
         get {
             if (object.ReferenceEquals(resourceMan, null)) {
-                global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BuiltinAssets", typeof(BuiltinAssets).Assembly);
+                global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Assets", typeof(Assets).Assembly);
                 resourceMan = temp;
             }
             return resourceMan;
@@ -62,9 +62,9 @@ internal class BuiltinAssets {
     /// <summary>
     ///   Looks up a localized resource of type System.Byte[].
     /// </summary>
-    internal static byte[] font_shared_tech_mono {
+    internal static byte[] FontSharedTechMono {
         get {
-            object obj = ResourceManager.GetObject("font_shared_tech_mono", resourceCulture);
+            object obj = ResourceManager.GetObject("FontSharedTechMono", resourceCulture);
             return ((byte[])(obj));
         }
     }
